aed02f08b9521da85f808a487c00df1f23857f40
BEN1JEN <benpheard@gmail.com>
2019-08-11 18:54:25 -0600

    Polish physics

--- func/player.lua
+++ func/player.lua
@@ -1,10 +1,19 @@
 local player = {}
 local collision = require "func.collision"
 
+local drag = 0.02
+local jumpVelocity = 30
+local airAcceleration = 5
+local groundAcceleration = 50
+local gravity = 90
+
 function player.new(colour)
 	return setmetatable({
 		collider = collision.new(2, 2),
-		colour = colour
+		colour = colour,
+		onGround = false,
+		onLeftWall = false,
+		onRightWall = false
 	}, {__index=player})
 end
 
@@ -14,17 +23,24 @@ function player:warpTo(x, y)
 end
 
 function player:updateLocal(world, cam, delta, k) -- update as if main player
-	self.collider.vy = self.collider.vy - 20*delta -- gravity
+	self.collider.vy = self.collider.vy - gravity*delta -- gravity
 	if k.playerLeft.held then
-		self.collider.vx = self.collider.vx - 20*delta
+		self.collider.vx = self.collider.vx - (self.onGround and groundAcceleration or airAcceleration)*delta
 	end
 	if k.playerRight.held then
-		self.collider.vx = self.collider.vx + 20*delta
+		self.collider.vx = self.collider.vx + (self.onGround and groundAcceleration or airAcceleration)*delta
 	end
-	if k.playerJump.down then
-		self.collider.vy = 5
+	if self.onGround and k.playerJump.down then
+		self.collider.vy = jumpVelocity
 	end
+
+	self.collider.vx, self.collider.vy = self.collider.vx * drag^delta, self.collider.vy * drag^delta
+
+	local ovx, ovy = self.collider.vx, self.collider.vy
 	self.collider:slide(world, delta)
+	self.onGround =  self.collider.vy == 0 and ovy < 0
+	self.onRightWall = self.collider.vx == 0 and ovx < 0
+	self.onLeftWall = self.collider.vx == 0 and ovx > 0
 
 	cam:glideTo(self.collider.x, self.collider.y, 10^delta)
 end
@@ -35,14 +51,6 @@ function player:draw(world, cam)
 	local sx, sy = cam:toScreenPosition(self.collider.x-1, self.collider.y+1)
 	love.graphics.rectangle("fill", sx, sy, ss, ss)
 	love.graphics.setColor(1, 1, 1)
-	local sx, sy = cam:toScreenPosition(self.collider.x, self.collider.y)
-	local ex, ey = cam:toScreenPosition(self.collider.x+self.collider.vx/2, self.collider.y+self.collider.vy/2)
-	love.graphics.line(sx, sy, ex, ey)
-	local possibleTiles = self.collider:getPossibleCollisions(world, self.collider.vx/2, self.collider.vy/2)
-	for _, t in ipairs(possibleTiles) do
-		local sx, sy = cam:toScreenPosition(t.x, t.y)
-		love.graphics.rectangle("line", sx, sy, 64, 64)
-	end
 end
 
 return player
