dac60b6bca21369df9cee8430eb3efc3fae432ac
BEN1JEN <benpheard@gmail.com>
2019-08-27 15:47:04 -0600

    Add test particle system, fix bugs

--- func/particles.lua
+++ func/particles.lua
@@ -9,15 +9,16 @@ function particles.new()
 end
 
 function particles:add(folder)
-	local p = require("assets." .. tostring(folder) .. ".metadata")
+	local p = require("assets.particles." .. tostring(folder) .. ".metadata")
 	for i, t in ipairs(p.textures) do
-		p.textures[i] = love.graphics.newImage("assets/" .. tostring(folder) .. tostring(t))
+		p.textures[i] = love.graphics.newImage("assets/particles/" .. tostring(folder) .. tostring(t))
+		p.textures[i]:setFilter("nearest", "nearest")
 	end
 	self.loaded[folder] = p
 end
 
 local function rnd(range)
-	return (math.random()*2-1)*rnd
+	return (math.random()*2-1)*range
 end
 
 function particles:instance(path, x, y)
@@ -34,7 +35,7 @@ function particles:instance(path, x, y)
 		table.insert(self.active, {
 			x = x, y = y,
 			vx = s.xVelocity+rnd(s.xVelocityRandom), vy = s.yVelocity+rnd(s.yVelocityRandom),
-			timeLeft = lifeSpan,
+			timeLeft = s.lifeSpan,
 			r = rotation,
 			rv = rnd(s.rotationVelocity),
 			drag = s.drag,
@@ -46,8 +47,8 @@ end
 
 function particles:update(delta)
 	for id, p in pairs(self.active) do
-		p.vy = p.vy + p.weight * delta
-		p.vx, p.vy = p.vx * p.drag^delta, o.vy * p.drag^delta
+		p.vy = p.vy - p.weight * delta
+		p.vx, p.vy = p.vx * p.drag^delta, p.vy * p.drag^delta
 		p.x, p.y = p.x + p.vx*delta, p.y + p.vy*delta
 		p.r = p.rv*delta
 		p.timeLeft = p.timeLeft - delta
@@ -58,7 +59,11 @@ function particles:update(delta)
 end
 
 function particles:draw(cam)
-
+	for _, p in pairs(self.active) do
+		local sx, sy = cam:toScreenPosition(p.x, p.y)
+		local ss = cam.scale/8
+		love.graphics.draw(p.tex, sx, sy, p.r, ss, ss)
+	end
 end
 
 return particles
