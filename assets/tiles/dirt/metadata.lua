8b2abcb058de2cfaacad476d48bdb627d7ff4d0b
Saweron <sam@finesketch.ca>
2019-08-30 11:24:33 -0600

    Replaced small test tiles with dirt

--- assets/tiles/dirt/metadata.lua
+++ assets/tiles/dirt/metadata.lua
@@ -9,21 +9,22 @@ return {
     {
 		uuid = "888c51cf-1880-4b9d-9ae1-e151f8dd2ba0",
 		texture = "/dirtTop1.png",
-		center = {x=0, y=0},
+		center = {x=0, y=1},
 		collisionOnBlock = true,
 		collisionOnExtra = false 
     },
     {
 		uuid = "5da1f15f-76bb-4680-8f5a-b51173ac4ce4",
 		texture = "/dirtTop2.png",
-		center = {x=0, y=0},
+		center = {x=0, y=1},
 		collisionOnBlock = true,
-		collisionOnExtra = false 
+		collisionOnExtra = false, 
+		renderRule = "foreground"
     },
     {
 		uuid = "43ca8001-150f-4915-85c3-b8f411363764",
 		texture = "/dirtTop3.png",
-		center = {x=0, y=0},
+		center = {x=0, y=1},
 		collisionOnBlock = true,
 		collisionOnExtra = false 
     }
--- func/terrainGen.lua
+++ func/terrainGen.lua
@@ -1,8 +1,8 @@
 local terrainGen = {}
 
 local uuidMap = {
-	testTiles_simple = "578a7491-c211-49e1-8704-0c08331bc667",
-	testTiles_grass = "7510ba13-f92c-4e73-86cc-e6e19d20159a",
+	testTiles_dirtInside = "dfba9564-c7a6-4b69-86d5-dfa92bbd2612",
+	testTiles_dirtTop = "5da1f15f-76bb-4680-8f5a-b51173ac4ce4",
 	testTiles_car = "584a1a1c-19ff-4e23-8cae-cd03b9491a6e",
 	testTiles_big = "64b72878-c4be-4e9c-8ba8-37e3654de003",
 }
@@ -26,7 +26,7 @@ function terrainGen:generateNext(world, amount)
 	for y = self.lastX, self.lastX+amount do
 		if y < 50 then
 			for x = 1, world.width do
-				world:setBlock(x, y, world.tileset.uuids[uuidMap[math.random() > 0.99 and "testTiles_car" or (y == 49 and "testTiles_grass" or "testTiles_simple")]])
+				world:setBlock(x, y, world.tileset.uuids[uuidMap[math.random() > 0.99 and "testTiles_car" or (y == 49 and "testTiles_dirtTop" or "testTiles_dirtInside")]])
 			end
 		elseif y == 50 then
 			world:setBlock(48, y, world.tileset.uuids[uuidMap["testTiles_big"]])
